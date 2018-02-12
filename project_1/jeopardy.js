694cb4fa5c0f06353255ee5f870acaa55a9d7b0a
Qya Ahmad <raqayyah@msn.com>
2018-02-12 16:42:52 -0500

    testing functions

--- project_1/jeopardy.js
+++ project_1/jeopardy.js
@@ -1,33 +1,51 @@
-var gagaForGa100 = document.querySelector('#cat1.100')
+var gagaForGa100 = function () {
     Question: How many jars of crunchy peanut butter containers are put out daily?
     Answer: '2'
     opt1: '3'
     opt2:'2'
     opt3:'1'
-
-
-var gagaForGa200 = document.querySelector('#cat1.200')
+    cat: 'gagaForGa'
+    loc: '#cat1.100'
+}
+var gagaForGa200 = function () {
     Question: What caused the fire alarm to sound last time it went off?
     Answer: 'Burning Popcorn'
     opt1: 'Burning Bread'
     opt2: 'Burning Plastic'
     opt3: 'Burning Popcorn'
+    cat: 'gagaForGa'
+    tile: '#cat1.200'
 
-var gagaForGa300 = document.querySelector('#cat1.300')
+var gagaForGa300 = function() { 
     Question: What animal does the robot near the bathroom claim to be?
     Answer: 'Unicorn'
     opt1: 'Unicorn'
     opt2: 'Bunny Rabbit'
     opt3: 'Puppy'
-
-var gagaForGa400 = document.querySelector('#cat1.400')
-    Question: What time does the office close for students? 
-    Answer: '10'
-    opt1: '11'
-    opt2: '10:30'
-    opt3: '10'
+    cat: 'gagaForGa'
+    tile: '#cat1.300'
 
 
+// var gagaForGa400 = document.querySelector('#cat1.400')
+//     Question: What time does the office close for students? 
+//     Answer: '10'
+//     opt1: '11'
+//     opt2: '10:30'
+//     opt3: '10'
+
+// var starWars100 = document.querySelector('#cat2.100')
+//     Question: What are the names of the twins on star wars?
+//     Answer: 'Luke & Leia'
+//     opt1: 'Hans & Leia'
+//     opt2: 'Luke & Leslie'
+//     opt3: 'Christian & Anastasia'
+
+// var starWars200 = document.querySelector('#cat2.200')
+//     Question: Hans solo had one main fighting companion. What was their name? 
+//     Answer: 
+//     opt1: 'Hans & Leia'
+//     opt2: 'Luke & Leslie'
+//     opt3: 'Christian & Anastasia'
 
 
 // let instructors = ["meg", "zakk", "perry", "james", "ali", "max", "mike"]
