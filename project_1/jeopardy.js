77ed4c168f06bda38575d4187a54ba792cd8a113
Qya Ahmad <raqayyah@msn.com>
2018-02-12 15:45:17 -0500

    game logic start

--- project_1/jeopardy.js
+++ project_1/jeopardy.js
@@ -1,1 +1,42 @@
-console.log("hello world")
+var gagaForGa100 = document.querySelector('#cat1.100')
+    Question: How many jars of crunchy peanut butter containers are put out daily?
+    Answer: '2'
+    opt1: '3'
+    opt2:'2'
+    opt3:'1'
+
+
+var gagaForGa200 = document.querySelector('#cat1.200')
+    Question: What caused the fire alarm to sound last time it went off?
+    Answer: 'Burning Popcorn'
+    opt1: 'Burning Bread'
+    opt2: 'Burning Plastic'
+    opt3: 'Burning Popcorn'
+
+var gagaForGa300 = document.querySelector('#cat1.300')
+    Question: What animal does the robot near the bathroom claim to be?
+    Answer: 'Unicorn'
+    opt1: 'Unicorn'
+    opt2: 'Bunny Rabbit'
+    opt3: 'Puppy'
+
+var gagaForGa400 = document.querySelector('#cat1.400')
+    Question: What time does the office close for students? 
+    Answer: '10'
+    opt1: '11'
+    opt2: '10:30'
+    opt3: '10'
+
+
+
+
+// let instructors = ["meg", "zakk", "perry", "james", "ali", "max", "mike"]
+// let currentInstructor = prompt('Who is currently teaching?')
+
+// for (let i = 0; i < instructors.length; i++) {
+//   if (instructors[i] === currentInstructor) {
+//     console.log(instructors[i] + ' is currently teaching!')
+//   } else {
+//     console.log(instructors[i])
+//   }
+// }
